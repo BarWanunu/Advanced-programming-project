0121a911f5978001388bb31bf25cb69c6122e5f0
BarWanunu <barwanunu6@gmail.com>
2024-01-13 17:01:28 +0200

    adding cMakeList and src, tests folders

---
+++ tests/calc_test.cpp
@@ -0,0 +1,6 @@
+#include <gtest/gtest.h>
+#include "D:\Advanced systems programming\מטלות פרוייקט\Advanced-programming-project\src\calc.cpp"
+
+TEST(SumTest, BasicTest) {
+EXPECT_EQ(sum(1,2), 3);
+}
