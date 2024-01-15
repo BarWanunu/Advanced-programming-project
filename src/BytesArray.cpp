511dfd30a0d15ea218203cc5f3332b0e46d11dab
Itay <itay17283@gmail.com>
2024-01-15 22:31:34 +0200

    update itay

--- src/BytesArray.cpp
+++ src/BytesArray.cpp
@@ -1,8 +1,9 @@
-class BytesArray; {
-    private:
-    int size;
+#include <iostream>
 
-    public :
+class BytesArray {
+    private:
+     int size;
+    public:
     int *byteArr;
     //constructor for the size of the array
     BytesArray(int n) : size(n), byteArr(new int[n]) {
@@ -10,26 +11,39 @@ class BytesArray; {
             byteArr[i] = 0;
         }
     }
-}
 
-//check if the asked index is 1 or 0
-bool checkIfIn(int index){
-    if (byteArr[index] == 1){
-        return true;
-    }
-    return false;
-}
-
-//change the bytes in the array if needed
-void insertURL (int index1, int index2){
-    if (byteArr[index1] == 0){
-        byteArr[index1] == 1;
-    }
-    if (byteArr[index2] == 0){
-        byteArr[index2] == 1;
+    //check if the asked index is 1 or 0
+    bool checkIfIn(int index){
+        if (byteArr[index] == 1){
+            return true;
+        }
+        return false;
     }
 
-}
+    //change the bytes in the array if needed (after the user entered the url)
+    void insertURL (int index1, int index2){
+        if (byteArr[index1] == 0){
+            byteArr[index1] == 1;
+        }
+        if (byteArr[index2] == 0){
+            byteArr[index2] == 1;
+        }
+    }
 
+    // Function to display the elements of the array
+    void display() {
+        for (int i = 0; i < size; i++) {
+            std::cout << byteArr[i] << " ";
+        }
+        std::cout << std::endl;
+    }
+
+};
+int main() {
+    BytesArray myArray(5);
+    myArray.display();
+	return 0;
+    
+}
 
 
--- src/hello.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-
-int main()
-{
-	// Hello world
-    std::cout << "Hello World!\n";
-	return 0;
-}
