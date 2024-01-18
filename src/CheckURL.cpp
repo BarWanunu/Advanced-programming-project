9dce8a2b055d3cd99c57960b53b24704a4d3c284
Itay <itay17283@gmail.com>
2024-01-18 19:45:52 +0200

    fix URLlist

--- src/CheckURL.cpp
+++ src/CheckURL.cpp
@@ -11,13 +11,13 @@ using namespace std;
 class CheckURL:public ICommand{
     public :
         CheckURL();
-        void execute(BytesArray arr, URLlist list, String url, HashFunction hash){
+        void execute(BytesArray arr, URLlist list, string url, HashFunction hash){
             //vector of 1 or 2 insexes
             vector<int> indexVector=hash.getHashes(url);
             for (int i=0; i<indexVector.size(); i++){
                 //check if the byte is 1
                 if (arr.checkIfIn(indexVector[i])){
-                    //if the byte is 1 chekc if the url is in the list
+                    //if the byte is 1 check if the url is in the list
                     if (list.contains(url)){
                         cout << "true true" << endl;
                         continue;
@@ -28,5 +28,4 @@ class CheckURL:public ICommand{
                 cout << "false" << endl;
             }
         }
-
 };
