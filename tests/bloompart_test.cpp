7f578f1626fba8b061046973ab677dff9ff9c81a
BarWanunu <barwanunu6@gmail.com>
2024-01-18 14:25:04 +0200

    making an adding test class

--- tests/bloompart_test.cpp
+++ tests/bloompart_test.cpp
@@ -20,31 +20,3 @@ TEST(BloomPartTest, Constructor) {
   EXPECT_EQ(3, bp2.numHashes());
 }
 
-// Test the add function.
-TEST(BloomPartTest, Adding) {
-    BloomPart bp = new BloomPart(8, 1);
-    string s = "www.example1.com";
-    bp.add(s);
-    EXPECT_EQ(bp.isBlack(s), true);
-}
-
-// Test the isBlack function.
-TEST(BloomPartTest, isBlack) {
-    BloomPart bp = new BloomPart(8, 1);
-    string s = "www.example1.com";
-    bp.add(s);
-    EXPECT_EQ(bp.isBlack(s), true);
-    EXPECT_EQ(bp.isInURL(s), true);
-    EXPECT_EQ(bp.isBlack("www.example2.com"), false);
-    EXPECT_EQ(bp.isBlack("www.example3.com"), false);
-}
-
-// Test the isInURL function.
-TEST(BloomPartTest, isInURL) {
-    BloomPart bp = new BloomPart(8, 1);
-    string s = "www.example1.com";
-    bp.add(s);
-    EXPECT_EQ(bp.isInURL(s), true);
-    EXPECT_EQ(bp.isInURL("www.example2.com"), false);
-    EXPECT_EQ(bp.isInURL("www.example3.com"), false);
-}
