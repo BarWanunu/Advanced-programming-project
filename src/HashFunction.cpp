16f153236431602543aee2b11b9f5a99c047eff9
BarWanunu <barwanunu6@gmail.com>
2024-01-22 20:10:09 +0200

    minor change of HashFunction.cpp

--- src/HashFunction.cpp
+++ src/HashFunction.cpp
@@ -17,12 +17,13 @@ vector<int> HashFunction::getHashes(string key) {
     hash<string> hashString1;
     hash<string> hashString2;
 
-    size_t hashValue;
-    string value;
+    // defining the hashValue and value outside the for loops
+    size_t hashValue = hashString1(key);
+    string value = to_string(hashValue);
 
     // first hash function
-    for (int i = 0; i < numHashes1; i++) {
-        hashValue = hashString1(key);
+    for (int i = 1; i < numHashes1; i++) {
+        hashValue = hashString1(value);
         value = to_string(hashValue);
     }
     //pushing the value modulu the size of the array
@@ -32,13 +33,17 @@ vector<int> HashFunction::getHashes(string key) {
      * a case when the functions will make the same number of hashes on the
      * key, we can return the vector because the values will be the same
     */
-     if (numHashes1 == numHashes2) {
+    if (numHashes1 == numHashes2) {
         return hashes;
     }
     //second hash function
     if (numHashes2 > 0) {
+
+        size_t hashValue = hashString2(key);
+        string value = to_string(hashValue);
+
         for (int i = 0; i < numHashes2; i++) {
-            hashValue = hashString2(key);
+            hashValue = hashString2(value);
             value = to_string(hashValue);
         }
         //pushing the value modulu the size of the array
